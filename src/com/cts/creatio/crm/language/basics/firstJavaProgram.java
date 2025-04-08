daf735dbbc1b4c9f0a5e247226818fcc9a988634
SamaKrupanandReddy <samakrupanandreddy@gmail.com>
2025-04-08 21:52:34 +0530

    Assignments

---
+++ src/com/cts/creatio/crm/language/basics/firstJavaProgram.java
@@ -0,0 +1,12 @@
+package com.cts.creatio.crm.language.basics;
+
+public class firstJavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println(2+2);
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
