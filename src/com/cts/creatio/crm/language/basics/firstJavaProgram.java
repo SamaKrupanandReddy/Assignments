4d9ede195d83590cabefe15395c64cf6ff0144ec
SamaKrupanandReddy <samakrupanandreddy@gmail.com>
2025-04-08 23:43:32 +0530

    Data types Assignemet

--- src/com/cts/creatio/crm/language/basics/firstJavaProgram.java
+++ src/com/cts/creatio/crm/language/basics/firstJavaProgram.java
@@ -4,7 +4,7 @@ public class firstJavaProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println(2+2);
+		System.out.println(2 + 2);
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 	}
